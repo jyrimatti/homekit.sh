4a17f107d1d91f019e523fe5379f7c3eb4067f69
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-03-01 22:41:45 +0200

    fix iid calculation for long typecodes

--- util/iid_service.sh
+++ util/iid_service.sh
@@ -6,11 +6,13 @@ set -eu
 
 logger_trace 'util/iid_service.sh'
 
-# use Service InstanceID from json if provided, or use its typecode converted to decimal + 10000 + $offset*1000
+# use Service InstanceID from json if provided, or use its typecode (first 8 chars) converted to decimal + 10000 + $offset*1000
 
 servicejson="$1"
 typecode="$2"
 index="$3" # index of this service amongst other services of the same type
 
-calculated="$((10000 * $(printf "%d\n" 0x"$typecode") + 1000 * index))"
+prefix=${typecode%????????}
+
+calculated="$((10000 * $(printf "%d\n" 0x"$prefix") + 1000 * index))"
 echo "$servicejson" | jq -ce ".iid // if .type == \"AccessoryInformation\" then 1 else $calculated end"
