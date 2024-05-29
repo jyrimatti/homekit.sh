246b916c8717f27e656ce00ad56122ca079dbabc
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-05-29 23:13:28 +0300

    --keep LD_LIBRARY_PATH

--- pairing/sign.sh
+++ pairing/sign.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p dash coreutils nix "pkgs.callPackage ./wolfclu.nix {}"
+#! nix-shell --pure --keep LD_LIBRARY_PATH -i dash -I channel:nixos-23.11-small -p dash coreutils nix "pkgs.callPackage ./wolfclu.nix {}"
 . ./prelude
 set -eu
 
--- reset.sh
+++ reset.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p dash nix jq ncurses which "pkgs.callPackage ./wolfclu.nix {}"
+#! nix-shell --pure --keep LD_LIBRARY_PATH -i dash -I channel:nixos-23.11-small -p dash nix jq ncurses which "pkgs.callPackage ./wolfclu.nix {}"
 . ./prelude
 set -eu
 
