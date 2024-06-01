37b697a83276de9d4f586e13d15e4016c0aeaeb6
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-01 21:36:20 +0300

    whoops

--- broadcast.sh
+++ broadcast.sh
@@ -7,9 +7,9 @@ dash ./util/bridges.sh \
     | {
         while read -r port bridge username; do {
             if [ "$port" = "" ]; then
-                echo ./broadcast-single.sh "$HOMEKIT_SH_PORT" &
+                dash ./broadcast-single.sh "$HOMEKIT_SH_PORT" &
             else
-                echo ./broadcast-single.sh "$port" "${bridge:-$port}" &
+                dash ./broadcast-single.sh "$port" "${bridge:-$port}" &
             fi
         } done
         wait $(jobs -p)
