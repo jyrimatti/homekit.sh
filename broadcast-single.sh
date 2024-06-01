d558bd9d9dac5510dfbe03e272e0e7bf3ac754cb
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-01 21:28:42 +0300

    ...

--- broadcast-single.sh
+++ broadcast-single.sh
@@ -13,10 +13,10 @@ fi
 while true
 do
     if command -v dns-sd >/dev/null; then
-        logger_info "Broadcasting with dns-sd"
+        logger_info "Broadcasting with dns-sd, bridge: ${bridge:-homekit.sh}"
         dns-sd -R "homekit.sh$bridge" _hap._tcp . "$port" $(cat "${HOMEKIT_SH_STORE_DIR}${bridge}/dns-txt") &
     else
-        logger_info "Broadcasting with avahi"
+        logger_info "Broadcasting with avahi, bridge: ${bridge:-homekit.sh}"
         avahi-publish -s "homekit.sh$bridge" _hap._tcp "$port" $(cat "$HOMEKIT_SH_STORE_DIR${bridge}/dns-txt") &
     fi
     DNSSD_PID=$!
@@ -24,10 +24,10 @@ do
     trap 'kill $DNSSD_PID' EXIT
 
     ret="$(fswatch -1 -m poll_monitor "$HOMEKIT_SH_STORE_DIR${bridge}/dns-txt")"
-    logger_info "dns-txt was modified"
+    logger_info "dns-txt was modified for bridge: ${bridge:-homekit.sh}"
     kill $DNSSD_PID
     if [ "$ret" = "" ]; then
-        logger_info "exiting broadcast.sh..."
+        logger_info "exiting broadcast.sh for bridge: ${bridge:-homekit.sh}..."
         exit 1
     fi
 done
