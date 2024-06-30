47c9528fc11871ef580acc9e85a4144997fb6c2a
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-30 11:49:16 +0300

    poll only pollable

--- util/poll.sh
+++ util/poll.sh
@@ -12,54 +12,56 @@ subscription_path="$HOMEKIT_SH_RUNTIME_DIR/sessions/$session/subscriptions/$subs
 if test -f "$subscription_path"; then
     logger_debug "Polling $subscription_path"
 
-    age="$(($(date +%s) - $(date +%s -r "$subscription_path")))"
-    previous_value="$(cat "$subscription_path")"
-
-    logger_debug "Got age $age"
-
-    aid="$(echo "$subscription" | cut -d . -f 1)"
-    iid="$(echo "$subscription" | cut -d . -f 2)"
+    aid="${subscription%%.*}"
+    iid="${subscription##*.}"
 
     service_with_characteristic="$(dash ./util/service_with_characteristic.sh "$aid" "$iid")"
     polling="$(echo "$service_with_characteristic" | jq -r '.characteristics[0].polling // .polling // empty')"
 
-    toString() {
-        servicename="$(echo "$service_with_characteristic"\
-                        | jq -r '.type'\
-                        | xargs dash ./util/type_to_string.sh)"
-        characteristicname="$(echo "$service_with_characteristic"\
-                               | jq -r '.characteristics[0].type'\
-                               | xargs dash ./util/type_to_string.sh)"
-        echo "$aid.$iid ($servicename.$characteristicname)"
-    }
+    if [ "$polling" != '' ]; then
+        toString() {
+            servicename="$(echo "$service_with_characteristic"\
+                            | jq -r '.type'\
+                            | xargs dash ./util/type_to_string.sh)"
+            characteristicname="$(echo "$service_with_characteristic"\
+                                | jq -r '.characteristics[0].type'\
+                                | xargs dash ./util/type_to_string.sh)"
+            echo "$aid.$iid ($servicename.$characteristicname)"
+        }
 
-    if [ "$previous_value" = '' ] || { [ "$polling" != '' ] && [ "$age" -gt "$polling" ]; }; then
-        touch "$subscription_path"
-        logger_debug 'Reading new value'
-        set +e
-        value="$(echo "$service_with_characteristic" | dash ./util/value_get.sh "$aid" "$iid")"
-        responsevalue=$?
-        set -e
-        if [ $responsevalue = 158 ]; then
-            logger_error "Got timeout while reading value for $(toString)"
-        elif [ $responsevalue = 152 ]; then
-            logger_error "Got empty response while reading value for $(toString)"
-        elif [ $responsevalue = 153 ]; then
-            logger_error "Got invalid response while reading value for $(toString)"
-        elif [ $responsevalue != 0 ]; then
-            logger_error "Got errorcode $responsevalue while reading value for $(toString)"
-        else
-            logger_info "Successfully polled value $value for $(toString)"
-            if [ "$(echo "$service_with_characteristic" | jq -c '.characteristics[0].format')" = 'string' ]; then
-                logger_debug 'Value is a string -> wrap it in quotes if not already'
-                value="$(echo "$value" | sed 's/^[^"].*[^"]$/"\0"/')"
-            fi
-            if [ "$value" != "$previous_value" ] && test -f "$subscription_path"; then
-                logger_debug "Creating event for $aid $iid"
-                echo "$value" > "$subscription_path"
-                tmpfile="$(mktemp "$HOMEKIT_SH_RUNTIME_DIR/homekit.sh_poll.XXXXXX")"
-                dash ./util/event_create.sh "$aid" "$iid" "$value" > "$tmpfile"
-                mv "$tmpfile" "$HOMEKIT_SH_RUNTIME_DIR/sessions/$session/events/$subscription.json"
+        age="$(($(date +%s) - $(date +%s -r "$subscription_path")))"
+        previous_value="$(cat "$subscription_path")"
+
+        logger_debug "Got age $age"
+
+        if [ "$previous_value" = '' ] || [ "$age" -gt "$polling" ]; then
+            touch "$subscription_path"
+            logger_debug 'Reading new value'
+            set +e
+            value="$(echo "$service_with_characteristic" | dash ./util/value_get.sh "$aid" "$iid")"
+            responsevalue=$?
+            set -e
+            if [ $responsevalue = 158 ]; then
+                logger_error "Got timeout while reading value for $(toString)"
+            elif [ $responsevalue = 152 ]; then
+                logger_error "Got empty response while reading value for $(toString)"
+            elif [ $responsevalue = 153 ]; then
+                logger_error "Got invalid response while reading value for $(toString)"
+            elif [ $responsevalue != 0 ]; then
+                logger_error "Got errorcode $responsevalue while reading value for $(toString)"
+            else
+                logger_info "Successfully polled value $value for $(toString)"
+                if [ "$(echo "$service_with_characteristic" | jq -c '.characteristics[0].format')" = 'string' ]; then
+                    logger_debug 'Value is a string -> wrap it in quotes if not already'
+                    value="$(echo "$value" | sed 's/^[^"].*[^"]$/"\0"/')"
+                fi
+                if [ "$value" != "$previous_value" ] && test -f "$subscription_path"; then
+                    logger_debug "Creating event for $aid $iid"
+                    echo "$value" > "$subscription_path"
+                    tmpfile="$(mktemp "$HOMEKIT_SH_RUNTIME_DIR/homekit.sh_poll.XXXXXX")"
+                    dash ./util/event_create.sh "$aid" "$iid" "$value" > "$tmpfile"
+                    mv "$tmpfile" "$HOMEKIT_SH_RUNTIME_DIR/sessions/$session/events/$subscription.json"
+                fi
             fi
         fi
     fi
